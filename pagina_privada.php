73420ca84e3a355aba4f312a87236208693ff6c5
Gian Piero Khalil Rodríguez Fádel <89124519+Crudo27@users.noreply.github.com>
2026-09-01 22:02:57 -0500

    Enlazando CRUD desde página privada

--- pagina_privada.php
+++ pagina_privada.php
@@ -16,11 +16,16 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Página privada</title>
+        <link rel="stylesheet" href="styles.css">
     </head>
 
     <body>
-        <h1>Bienvenido, <?= htmlspecialchars($nombreUsuario, ENT_QUOTES, 'UTF-8')?></h1>
+        <div class="privada-container">
+            <h1>Bienvenido, <?= htmlspecialchars($nombreUsuario, ENT_QUOTES, 'UTF-8')?></h1>
 
-        <p>Has iniciado sesión correctamente</p>
+            <p>Has iniciado sesión correctamente</p>
+
+            <a class="boton" href="usuarios.php">Gestionar usuarios</a>
+        </div>
     </body>
 </html>
